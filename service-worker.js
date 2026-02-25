0c15c13f5f63bd6323922dc4985950a3eb2d5b9c
sridhar96-alt <onigirizoro96@gmail.com>
2026-02-25 16:07:10 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,5 @@
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("smarthealth").then(cache => {
-      return cache.addAll(["./","index.html"]);
-    })
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
+self.addEventListener("fetch", e => {});
